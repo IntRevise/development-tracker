f16494e7fecbb077b8f320a1a4e22616226e1b9e
Axton <37771600+axtonprice@users.noreply.github.com>
2021-12-06 20:42:13 +0000

    Delete _partials directory

--- _partials/footer.php
+++
@@ -1,5 +0,0 @@
-<section class="footer">
-    <div class="copyright-content">
-        <p style="color:lightgray">Not affiliated with <b>Wootton Upper School</b> &copy; 2020-<?php echo date("Y"); ?></p>
-    </div>
-</section>
