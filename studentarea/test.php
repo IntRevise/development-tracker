bdb38353102c070720a191afdbc02b1be63bd36c
Axton <37771600+axtonprice@users.noreply.github.com>
2021-12-06 20:41:05 +0000

    Delete studentarea directory

--- studentarea/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require $_SERVER['DOCUMENT_ROOT'] . "/lab/int-revise/modules/_functions.php";
-session_start();
-
-file_get_contents("https://intrevise.axtonprice.com/api/v1/custom/isAccountLinkedDiscord?token=RLVGjdlGdRbDDUaktppVTUvnhmNadRMOZbrkBUcV&id=" . getData("accounts", "id", "username", "priroa"))
-?>
